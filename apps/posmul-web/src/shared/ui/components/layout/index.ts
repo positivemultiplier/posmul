64cc9a38d0418618484e8758b2a14938881a740b
positivemultiplier <posmul@naver.com>
2025-12-24 13:48:19 +0900

    refactor: 레거시 Navbar 파일 정리 및 테스트 파일 이동

--- apps/posmul-web/src/shared/ui/components/layout/index.ts
+++ apps/posmul-web/src/shared/ui/components/layout/index.ts
@@ -1,5 +1,4 @@
 // Layout Components
-export { default as Navbar } from "./Navbar";
-export * from "./Navbar";
+export { ThreeRowNavbar } from "./ThreeRowNavbar";
 export { MoneyWaveStatus } from "./MoneyWaveStatus";
 export * from "./CategoryOverviewLayout";
